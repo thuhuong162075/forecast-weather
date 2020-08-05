b5f0ab4b5118f1ea61f8edc5b8b2d376e83fafbe
Thu Huong <huong.ttt171298@gmail.com>
2020-08-05 17:02:30 +0700

    edit divide componnet

--- src/weather/pages/result/index.js
+++ src/weather/pages/result/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Infomation from '../../components/Infomation';
+import '../../assets/css/Infomation.css'
+import WeatherToday from '../../components/WeatherToday';
+import Forecast from '../../components/Forecast';
 
 function ResultWeather() {
   return (
     <div className="info-search">
-        <Infomation />
+      <div className="Infomation">
+        <div className="info-title">
+            <h1 className="city">Hà Nội</h1>
+            <p className="time">Monday 3 August</p>
+        </div>
+        <div className="info-content">
+            <WeatherToday />
+            <Forecast />
+        </div>
+      </div>
     </div>
   );
 }
