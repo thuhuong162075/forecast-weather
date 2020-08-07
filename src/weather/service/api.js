de519a86c75f70a3bbe7e73cf6874b9f096413ed
Thu Huong <huong.ttt171298@gmail.com>
2020-08-07 17:11:18 +0700

    edit api

--- src/weather/service/api.js
+++ src/weather/service/api.js
@@ -3,14 +3,14 @@ import callApi from './callApi';
 import * as Urls from '../constants/Config'
 
 const searchWeatherApi = async (nameCity) => {
-    const response = await callApi(`${Urls.proxy}${Urls.API_URL}${'weather?q='}${nameCity}${Urls.APPID}`, 
+    const response = await callApi(`${Urls.proxy}${Urls.API_URL}${'weather?q='}${nameCity}${'&APPID='}${Urls.APPID}${'&lang=vi'}`, 
     "GET", null)
     if(!response) return 1
     const result = await response.status === 200 ? response.data : 1
     return result
 }
 const searchForecastApi = async (nameCity) => {
-    const response = await callApi(`${Urls.proxy}${Urls.API_URL}${'forecast?q='}${nameCity}${Urls.APPID}`, 
+    const response = await callApi(`${Urls.proxy}${Urls.API_URL}${'forecast?q='}${nameCity}${'&APPID='}${Urls.APPID}${'&lang=vi'}`, 
     "GET", null)
     const result = await response.status === 200 ? response.data : {}
     return result
